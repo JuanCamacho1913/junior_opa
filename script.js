bd0a1569f692f23e53bf2e5251d59268c37faba4
¨juan <jecb1913@gmail.com>
2022-10-21 11:41:49 -0500

    Arreglo de posición de resultados

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
- function EsPerfecto(n) {
-     let sum = 1;
-  
-     for (let i = 2; i*i < n; i++){
-     if (n%i==0)
-     {
-       if(i*i!=n)
-         sum = sum + i + n/i;
-       else
-         sum=sum+i;
-     }
-   }
-  
-   if (sum == n && n != 1)
-     return true;
-   else
-     return false;
- }
+function EsPerfecto(n) {
+  let sum = 1;
 
+  for (let i = 2; i * i < n; i++) {
+    if (n % i == 0) {
+      if (i * i != n) sum = sum + i + n / i;
+      else sum = sum + i;
+    }
+  }
 
- function NumerosPerfectosRango(rango) {
-   for(let i = 1;i <= rango; i++){ 
-     if (EsPerfecto(i)){
-      document.write("<br/>")
-      document.write(i + ' Es número natural perfecto ')
-      document.write("<br/>")
-     }
-   }
- }
+  if (sum == n && n != 1) return true;
+  else return false;
+}
 
- NumerosPerfectosRango(rango = parseInt(prompt('Introduce un número para saber en ese rango números perfectos existentes ')));
+function NumerosPerfectosRango(rango) {
+  for (let i = 1; i <= rango; i++) {
+    if (EsPerfecto(i)) {
+      let createElement = document.createElement("div");
+      createElement.innerHTML = i + " es número natural perfecto";
+      document.body.appendChild(createElement);
+    }
+  }
+}
 
- 
- lista = [{
-  text: 'Números naturales perfectos',
-  
-}];
+NumerosPerfectosRango(
+  (rango = parseInt(
+    prompt(
+      "Introduce un número para saber en ese rango números perfectos existentes "
+    )
+  ))
+);
 
-localStorage.setItem('list', JSON.stringify(lista));
-lista = JSON.parse(localStorage.getItem('list'));
-lista.forEach(element => {
-  let elements = document.getElementById('elements');
-  let div = document.createElement('div');
+lista = [
+  {
+    text: "Números naturales perfectos",
+  },
+];
+
+localStorage.setItem("list", JSON.stringify(lista));
+lista = JSON.parse(localStorage.getItem("list"));
+lista.forEach((element) => {
+  let elements = document.getElementById("elements");
+  let div = document.createElement("h2");
   elements.appendChild(div);
-  div.innerHTML = element.text + ': '
+  div.innerHTML = element.text + ": ";
 });
- 
-
